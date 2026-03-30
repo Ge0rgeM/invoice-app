f5ebbd070a0260757313e5882fa14892d60b8941
Ge0rgeM <g_modebadze3@cu.edu.ge>
2026-03-30 14:32:48 +0400

    Uptaded Vite config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from 'path'
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
-    base: '/invoice-app/',
+    base: './',
     resolve: {
       alias: {
         '@': path.resolve(__dirname, './src'),
