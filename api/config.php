7dd70f3eacea7f36bed569dc0ca3e4dfdbe60310
Ge0rgeM <g_modebadze3@cu.edu.ge>
2026-03-30 23:34:05 +0400

    Stop tracking config.php

--- api/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-// api/config.php
-
-// Define your InfinityFree database credentials here
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'invoices_db');
